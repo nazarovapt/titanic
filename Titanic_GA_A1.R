6190fdedaee1753de93d3e3d418359f7fcae6b9f
Zerb314 <meicoheil@outlook.de>
2026-01-21 20:16:49 +0100

    Add files via upload
    Zwischenstand 21.01. Aufgabe 1

---
+++ Titanic_GA_A1.R
@@ -0,0 +1,33 @@
+# ============================================================
+# Titel:    Analyse der Verkaufsdaten
+# Datei:    analyse_verkauf.R
+# Autoren:  Meico Bastian Heil, Saskia Lapkowski
+# Datum:    2026-01-21
+# Zweck:    Datenaufbereitung und Auswertung
+# ============================================================
+
+#Datensatz einlesen
+titanic <- read.csv("~/titanic.csv")
+
+## 1.
+
+# Extrahiert aus dem Namen eine Variable mit der Anrede der Person, d.h.
+# „Mr.“, „Mrs.“, „Mse.“ usw., damit später fehlende Werte im Alter ersetzt
+# werden können. Beachtet hierbei, dass gewisse Anreden wie „Ms.“, „Miss.“
+# oder „Mlle“ inhaltlich gleichbedeutend sind (in diesem Beispiel eine junge,
+# unverheiratete Frau). Die Anrede „Master“ bezeichnet einen kleinen Jungen.
+
+?gsub
+?regex
+
+# Alles vor dem Komma wird ignoriert, dann beliebig viele Leerzeichen,
+# in () der Titel bis zum nächsten Punkt, alles nach dem Punkt wird ignoriert
+titanic$Anrede <- gsub(".*,\\s*([A-Za-z ]+\\.).*", "\\1", titanic$Name)
+unique(titanic$Anrede)
+
+# Anreden werden nach Alter und Geschlecht gruppiert
+young_woman <- titanic$Anrede %in% c("Miss.", "Ms.", "Mlle.")
+older_woman <- titanic$Anrede %in% c("Mrs.", "Mme.", "Lady.", "the Countess.")
+boys <- titanic$Anrede == "Master."
+adult_men <- titanic$Anrede %in% c("Mr.", "Don.", "Sir.", "Jonkheer.")
+
