d0b19d9d168e5cea37be22548dc65ffa8a6051df
Bagrinn96 <nazarovapt@gmail.com>
2026-01-27 23:16:34 +0100

    Update Titanic_GA_A1.R

--- Titanic_GA_A1.R
+++ Titanic_GA_A1.R
@@ -155,7 +155,7 @@ titanic <- titanic [, !names(titanic) %in% "Cabin"]
 titanic_new <- titanic
 
 # CSV-Datei erstellen, die den Datensatz beinhaltet
-write.csv(titanic_new, "titanic_new", row.names = FALSE)
+write.csv(titanic_new, "titanic_new.csv", row.names = FALSE)
 
 # =============================================================================
 
