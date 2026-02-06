c0d812a787da4b67363b0da697741389cf5cd39c
ticia369 <mleticiak2020@gmail.com>
2026-02-06 13:17:32 +0100

    weitere Hilfsfunktionen zur Variablenüberprüfung

--- scripts/helper_functions.R
+++ scripts/helper_functions.R
@@ -1,8 +1,8 @@
-# ============================================================================= 
+
 # Titel:    Hilfsfunktion zur deskriptiven Analyse und Visualisierung des 
 #           Titanic-Datensatzes
 # Autoren:  Leticia Mbong Kwanga
-# ============================================================================= 
+
 
 remove_na <- function(df, vars) {
   df[complete.cases(df[, vars]), ]
@@ -12,5 +12,24 @@ var_exist <- function(df, vars) {
   missing <- vars[!vars %in% names(df)]
   if (length(missing) > 0) {
     stop(paste("Folgende Variablen fehlen:", paste(missing, collapse = ", ")))
+  }
+}
+
+is_numeric <- function(x, varname = "") {
+  if (!is.numeric(x)) {
+    stop(paste("Variable", varname, "ist nicht numerisch."))
+  }
+}
+
+is_dichotom <- function(x, varname = "") {
+  x <- x[!is.na(x)]
+  if (length(levels(factor(x))) != 2) {
+    stop(paste("Variable", varname, "ist nicht dichotom."))
+  }
+}
+
+is_factor <- function(x, varname = "") {
+  if (!is.factor(x)) {
+    stop(paste("Variable", varname, "ist nicht kategorial (factor)."))
   }
 }
