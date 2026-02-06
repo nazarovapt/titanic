3e93b90b860eace624e24784a28f22af4405bf2b
ticia369 <mleticiak2020@gmail.com>
2026-02-06 13:09:19 +0100

    Update helper_functions.R
    Funktion zum Entfernen von NA's und Variablen Überprüfung

--- scripts/helper_functions.R
+++ scripts/helper_functions.R
@@ -3,3 +3,14 @@
 #           Titanic-Datensatzes
 # Autoren:  Leticia Mbong Kwanga
 # ============================================================================= 
+
+remove_na <- function(df, vars) {
+  df[complete.cases(df[, vars]), ]
+}
+
+var_exist <- function(df, vars) {
+  missing <- vars[!vars %in% names(df)]
+  if (length(missing) > 0) {
+    stop(paste("Folgende Variablen fehlen:", paste(missing, collapse = ", ")))
+  }
+}
